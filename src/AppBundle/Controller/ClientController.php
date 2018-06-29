0f4bd398988c7e09270d882c04bfd03e622aa6b8
smouheb <smael.mouheb@gmail.com>
2018-06-29 04:40:35 +0200

    adding a layer of security for all requests related to users

--- src/AppBundle/Controller/ClientController.php
+++ src/AppBundle/Controller/ClientController.php
@@ -9,6 +9,7 @@ use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ClientController extends FOSRestController
 {
@@ -22,8 +23,8 @@ class ClientController extends FOSRestController
     {
 
         $listOfUsers = $this->getDoctrine()
-                            ->getRepository('AppBundle:User')
-                            ->find($id);
+            ->getRepository('AppBundle:User')
+            ->find($id);
 
         return $listOfUsers;
 
@@ -37,6 +38,11 @@ class ClientController extends FOSRestController
      */
     public function listOfUserPerClientAction($id)
     {
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+
+            throw new AccessDeniedException();
+
+        }
         //Query pour aller recuperer tous les users lié à un Client
 
         $em = $this->getDoctrine()
@@ -65,6 +71,11 @@ class ClientController extends FOSRestController
      */
     public function postUserAction(Request $request, User $user)
     {
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+
+            throw new AccessDeniedException();
+
+        }
 
         $errors = $this->get('validator')->validate($user);
 
@@ -97,7 +108,7 @@ class ClientController extends FOSRestController
                         'id' => $user->getId(), UrlGeneratorInterface::ABSOLUTE_URL
 
                     ]
-               )//remonter de bug brakets to put behing getid()
+               )
            ]
        );
     }
@@ -110,6 +121,11 @@ class ClientController extends FOSRestController
      */
     public function deleteUserAction(User $user)
     {
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+
+            throw new AccessDeniedException();
+
+        }
 
         $em = $this->getDoctrine()
                    ->getManager();
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,7 +4,6 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use JMS\Serializer\Annotation as Serializer;
-use Symfony\Component\Validator\Constraints as Assert;
 use Hateoas\Configuration\Annotation as Hateoas;
 use FOS\UserBundle\Model\User as BaseUser;
 
